93cd119abe877c857d3d9cbb28777634820e79bb
Andrey Sobolev <copyprof@gmail.com>
2021-10-24 18:35:37 +0300

    Work

--- SobolevAA_HW2.2/ViewController.swift
+++ SobolevAA_HW2.2/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  SobolevAA_HW2.2
@@ -9,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ViewOutLet: UIView!
+    
+    @IBOutlet var RedSlider: UISlider!
+    @IBOutlet var GreenSlider: UISlider!
+    @IBOutlet var BlueSlider: UISlider!
+    
+    @IBOutlet var OutRed: UILabel!
+    @IBOutlet var OutGreen: UILabel!
+    @IBOutlet var OutBlue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Так удобнее
+        RedChanged(self)
     }
-
-
+    
+    @IBAction func RedChanged(_ sender: Any) {
+       
+        let redValue   = CGFloat(RedSlider.value)
+        let greenValue = CGFloat(GreenSlider.value)
+        let blueValue  = CGFloat(BlueSlider.value)
+               
+        ViewOutLet.backgroundColor = UIColor(red: redValue,
+                                             green: greenValue,
+                                             blue: blueValue,
+                                             alpha: 1)
+        
+        OutRed.text   = String(round(RedSlider.value   * 100) / 100)
+        OutGreen.text = String(round(GreenSlider.value * 100) / 100)
+        OutBlue.text  = String(round(BlueSlider.value  * 100) / 100)
+        
+        
+    }
+    
 }
 
