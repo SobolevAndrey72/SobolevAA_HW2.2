9427a6bb5c7aec0372b340e7f6cef70f2cf372fd
Andrey Sobolev <copyprof@gmail.com>
2021-10-24 17:35:47 +0300

    Initial Commit

---
+++ SobolevAA_HW2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SobolevAA_HW2.2
+//
+//  Created by Андрей Соболев on 24.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
